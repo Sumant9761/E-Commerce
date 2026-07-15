fb84f072eb151ac5ee83ca8de04f65169e4be47b
Sumant9761 <sumantkumaryadav7037@gmail.com>
2026-07-15 23:21:04 +0530

    Initial setup & build header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,114 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Search from "../Search";
+import Badge from "@mui/material/Badge";
+import IconButton from "@mui/material/IconButton";
+import { IoCart } from "react-icons/io5";
+import { IoIosGitCompare } from "react-icons/io";
+import { FaRegHeart } from "react-icons/fa";
+import Tooltip from "@mui/material/Tooltip";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="top-strip py-2 border-t-[1px] border-gray-300 border-b-[1px]">
+        <div className="container">
+          <div className="flex item-center justify-between">
+            <div className="col1 w-[50%]">
+              <p className="text-[14px] font-[500]">
+                Get up to 50% off new season styles, limited time only
+              </p>
+            </div>
+
+            <div className="col2 flex items-center justify-end">
+              <ul className="flex items-center gap-3">
+                <li className="list-none">
+                  <Link
+                    to="/help-center"
+                    className="text-[13px] link font-[500] transition"
+                  >
+                    Help Center
+                  </Link>
+                </li>
+                <li className="list-none">
+                  <Link
+                    to="/order-tracking"
+                    className="text-[13px] link font-[500] transition"
+                  >
+                    Order Tracking
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="header py-3">
+        <div className="container flex items-center justify-between">
+          <div className="col1 w-[20%]">
+            <Link to={"/"}>
+              <img src="/logo.jpg" />
+            </Link>
+          </div>
+
+          <div className="col2 w-[45%]">
+            <Search />
+          </div>
+
+          <div className="col3 w-[30%] flex items-center pl-7">
+            <ul className="flex items-center justify-end gap-3 w-full">
+              <li className="list-one">
+                <Link
+                  to="/login"
+                  className="link transition text-[15px] font-[500]"
+                >
+                  Login
+                </Link>{" "}
+                | &nbsp;
+                <Link
+                  to="/register"
+                  className="link transition text-[15px] font-[500]"
+                >
+                  Register
+                </Link>
+              </li>
+
+              <li>
+                <Tooltip title="Compare">
+                  <IconButton aria-label="cart">
+                    <Badge badgeContent={8} color="primary">
+                      <IoIosGitCompare />
+                    </Badge>
+                  </IconButton>
+                </Tooltip>
+              </li>
+
+              <li>
+                <Tooltip title="Wishlist">
+                  <IconButton aria-label="cart">
+                    <Badge badgeContent={8} color="primary">
+                      <FaRegHeart />
+                    </Badge>
+                  </IconButton>
+                </Tooltip>
+              </li>
+
+              <li>
+                <Tooltip title="Cart">
+                  <IconButton aria-label="cart">
+                    <Badge badgeContent={8} color="primary">
+                      <IoCart />
+                    </Badge>
+                  </IconButton>
+                </Tooltip>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
